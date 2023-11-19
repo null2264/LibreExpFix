eb640a5d17e9b3af63aaf7cdf4e329373715c323
ziro <palembani@gmail.com>
2023-11-19 14:07:16 +0700

    chore: Adjust repo position

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
         maven("https://maven.aap.my.id/releases/")
         maven("https://maven.architectury.dev/")
         maven("https://jitpack.io/")
         maven("https://files.minecraftforge.net/maven/")
         maven("https://repo.essential.gg/repository/maven-public/")
-        gradlePluginPortal()
     }
     plugins {
         id("com.github.johnrengelman.shadow") version("8.1.1")
