4a97e2eda89b93418b70a7be8691308277d9a7e6
ziro <palembani@gmail.com>
2024-01-18 12:15:38 +0700

    refactor: Reflection because Forge is Forge

--- src/main/java/io/github/null2264/libreexpfix/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/io/github/null2264/libreexpfix/mixin/ServerPlayerEntityMixin.java
@@ -1,11 +1,14 @@
 package io.github.null2264.libreexpfix.mixin;
 
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundPlayerAbilitiesPacket;
 import net.minecraft.network.protocol.game.ClientboundSetExperiencePacket;
 import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,6 +16,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerEntityMixin
 {
+    @Unique
+    private void sendPacket(ServerGamePacketListenerImpl connection, Packet<?> packet) {
+        //#if FABRIC>=1
+        connection.send(packet);
+        //#else
+        // Workaround for Forge
+        String majorVersion = net.minecraft.DetectedVersion.BUILT_IN.getName().split("\\.")[1];
+        String func;
+        if (majorVersion.equals("18"))
+            func = "m_141995_";
+        else
+            func = "m_9829_";
+
+        java.lang.reflect.Method method;
+        try {
+            method = connection.getClass().getMethod(func, Packet.class);
+            method.invoke(connection, packet);
+        } catch (NoSuchMethodException | java.lang.reflect.InvocationTargetException | IllegalAccessException ignored) {
+            // Welp, nothing we can do
+        }
+        //#endif
+    }
+
     @Inject(method = "triggerDimensionChangeTriggers", at = @At("TAIL"))
     private void afterWorldChanged(ServerLevel origin, CallbackInfo ci) {
         /* Tests:
@@ -22,12 +48,12 @@ public abstract class ServerPlayerEntityMixin
          * - /execute in overworld run tp @s ~ 10 ~
          */
         ServerPlayer player = (ServerPlayer) (Object) this;
-        player.connection.send(new ClientboundSetExperiencePacket(
+        sendPacket(player.connection, new ClientboundSetExperiencePacket(
                 player.experienceProgress,
                 player.totalExperience,
                 player.experienceLevel
         ));
-        player.connection.send(new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
-        player.getActiveEffects().forEach(instance -> player.connection.send(new ClientboundUpdateMobEffectPacket(player.getId(), instance)));
+        sendPacket(player.connection, new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
+        player.getActiveEffects().forEach(instance -> sendPacket(player.connection, new ClientboundUpdateMobEffectPacket(player.getId(), instance)));
     }
 }
