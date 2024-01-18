b38159e620ea9d8cf590b07ef06be51d16b3f259
ziro <palembani@gmail.com>
2024-01-18 08:02:51 +0700

    revert: "Adjust mapping"
    This reverts commit d11f689986ec0968d7a4d5948a2cd67e79589586.

--- root.gradle.kts
+++ root.gradle.kts
@@ -14,8 +14,10 @@ preprocess {
     // In case CHASM released
     //val quilt11800 = createNode("1.18-quilt", 11800, "yarn")
 
-    neoforge12002.link(forge12002)
+    neoforge12002.link(fabric12002)
+
     forge12002.link(fabric12002)
-    fabric12002.link(forge11800)
     forge11800.link(fabric11800)
+
+    fabric12002.link(fabric11800)
 }
