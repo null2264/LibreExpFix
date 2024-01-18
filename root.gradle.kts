be599396736604589ae7ebea725d6fbd7b6001b2
ziro <palembani@gmail.com>
2024-01-18 08:05:41 +0700

    chore: Manual mapping

--- root.gradle.kts
+++ root.gradle.kts
@@ -17,7 +17,7 @@ preprocess {
     neoforge12002.link(fabric12002)
 
     forge12002.link(fabric12002)
-    forge11800.link(fabric11800)
+    forge11800.link(fabric11800, file("versions/1.18-forge/mappings.txt"))
 
     fabric12002.link(fabric11800)
 }
