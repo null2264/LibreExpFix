d758347d03195818f01cabb16a55dad199b674ad
ziro <palembani@gmail.com>
2024-01-18 09:52:41 +0700

    refactor: Remove redundant shadow

--- src/main/java/io/github/null2264/libreexpfix/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/io/github/null2264/libreexpfix/mixin/ServerPlayerEntityMixin.java
@@ -5,9 +5,7 @@ import net.minecraft.network.protocol.game.ClientboundSetExperiencePacket;
 import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,9 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerEntityMixin
 {
-    @Shadow
-    public ServerGamePacketListenerImpl connection;
-
     @Inject(method = "triggerDimensionChangeTriggers", at = @At("TAIL"))
     private void afterWorldChanged(ServerLevel origin, CallbackInfo ci) {
         /* Tests:
@@ -27,12 +22,12 @@ public abstract class ServerPlayerEntityMixin
          * - /execute in overworld run tp @s ~ 10 ~
          */
         ServerPlayer player = (ServerPlayer) (Object) this;
-        connection.send(new ClientboundSetExperiencePacket(
+        player.connection.send(new ClientboundSetExperiencePacket(
                 player.experienceProgress,
                 player.totalExperience,
                 player.experienceLevel
         ));
-        connection.send(new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
-        player.getActiveEffects().forEach(instance -> connection.send(new ClientboundUpdateMobEffectPacket(player.getId(), instance)));
+        player.connection.send(new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
+        player.getActiveEffects().forEach(instance -> player.connection.send(new ClientboundUpdateMobEffectPacket(player.getId(), instance)));
     }
 }
