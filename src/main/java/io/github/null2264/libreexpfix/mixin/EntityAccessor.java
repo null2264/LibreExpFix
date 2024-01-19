08b63518bdf6263b672cc96a510173700c2f6c58
ziro <palembani@gmail.com>
2024-01-19 09:00:44 +0700

    fix: Disabled the wrong mixin

--- src/main/java/io/github/null2264/libreexpfix/mixin/EntityAccessor.java
+++ src/main/java/io/github/null2264/libreexpfix/mixin/EntityAccessor.java
@@ -1,13 +1,13 @@
 //#if FORGE>=1
-package io.github.null2264.libreexpfix.mixin;
+//$$ package io.github.null2264.libreexpfix.mixin;
 
-import net.minecraft.world.entity.Entity;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+//$$ import net.minecraft.world.entity.Entity;
+//$$ import org.spongepowered.asm.mixin.Mixin;
+//$$ import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(Entity.class)
-public interface EntityAccessor {
-    @Accessor("id")
-    int getEntityId();
-}
+//$$ @Mixin(Entity.class)
+//$$ public interface EntityAccessor {
+//$$     @Accessor("id")
+//$$     int getEntityId();
+//$$ }
 //#endif
